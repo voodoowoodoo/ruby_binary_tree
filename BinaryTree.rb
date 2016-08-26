e4f35f7446d266ada82517f4110f56b9db663744
Jorrit <jorrit_l@hotmail.com>
2016-08-26 22:33:55 +0200

    Removed not working algorithm

--- BinaryTree.rb
+++ BinaryTree.rb
@@ -21,20 +21,8 @@ class BinaryTree
 
 	# Takes original array, and builds a BinaryTree object out of this array
 	def initialize(array)
-
-		# Sets the value of the root node to the first value in the array.
-		@root = Node.new(array[0])
-
 	end
-
-	# Takes a value and a node, and adds a new node with that value as its @value variable to the tree at the appropriate place in the tree.
-	def add(value, node = @root)
-		if value > node.value
-		#Code for adding node to the right
-		else
-		#Code for adding node to the left
-	end
-
+	
 	# Returns the node object containing value entered, searching the tree breadth first, using a queue.
 	def breadth_first_search(value)
 	end
